46400a59c8bd0730254efe4d96479243cd7d55d2
MarLeu <36078652+MarLeu@users.noreply.github.com>
2018-02-21 21:21:17 +0100

    Parser functions added

--- translator/parse.cpp
+++ translator/parse.cpp
@@ -2,23 +2,32 @@
 #include <string>
 #include <regex>
 #include <fstream>
+#include <sstream>
 
 using namespace std;
 
 string openFile(string filepath);
-string REP_import(string code);
-string REP_comments(string code);
+void REP_import(string * code);
+void REP_comments(string * code);
+void REP_function_void(string * code);
+void REP_for(string * code);
+void REP_variableDeclaration(string * code);
 
+void replace(string * code, string regexstring, string result);
 
 int main(int argc, char* argv[])
 {
 	string code = openFile(argv[1]);
 	
-	code = REP_comments(code);
-	code = REP_import(code);
+	REP_comments(&code);
+	REP_import(&code);
+	REP_function_void(&code);
+	REP_for(&code);
+	REP_variableDeclaration(&code);
 	
 	//Test-Output
 	cout << code;
+	return 0;
 }
 
 
@@ -42,13 +51,23 @@ string openFile(string filepath)
 
 //////////////////////////////////////////////////////////////
 //
+//  Basic replace function
+//
+//////////////////////////////////////////////////////////////
+void replace(string * code, string regexstring, string result)
+{
+	regex exp(regexstring);
+	*code = regex_replace(*code, exp, result);
+}
+
+//////////////////////////////////////////////////////////////
+//
 //  Replace import with #include
 //
 //////////////////////////////////////////////////////////////
-string REP_import(string code)
+void REP_import(string * code)
 {
-	regex exp("import (\\w+)");
-	return regex_replace(code, exp, "#include $1");
+	replace(code, "import (\\w+)", "#include $1");
 }
 
 //////////////////////////////////////////////////////////////
@@ -56,8 +75,39 @@ string REP_import(string code)
 //  Remove comments (not needed in cpp file)
 //
 //////////////////////////////////////////////////////////////
-string REP_comments(string code)
+void REP_comments(string * code)
 {
-	regex exp("[#\'(\\/\\/)](.+)");
-	return regex_replace(code, exp, "");
+	replace(code, "[#\'](.+)", "");
+}
+
+//////////////////////////////////////////////////////////////
+//
+//  Repace main function
+//  
+//////////////////////////////////////////////////////////////
+void REP_function_void(string * code)
+{
+	replace(code, "func \\w+(\\(.+\\))", "void Main $1");
+}
+
+
+//////////////////////////////////////////////////////////////
+//
+//  Create a C-Loop
+//  ToDo: Curly brackets
+//
+//////////////////////////////////////////////////////////////
+void REP_for(string * code)
+{
+	replace(code, "for \\((\\w+) ([\\w\\d]+) in (\\d+)\\.\\.\\.(\\d+)\\)", "for (int $1 = $2; $1 <= $3; $1++)");
+}
+
+//////////////////////////////////////////////////////////////
+//
+//  Variable declaration conversion
+//
+//////////////////////////////////////////////////////////////
+void REP_variableDeclaration(string * code)
+{
+	replace(code, "var (\\w+) : (\\w+)", "$2 $1");
 }
